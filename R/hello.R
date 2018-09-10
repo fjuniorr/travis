169a7ec830a05bb13d79b6c245d0cac8df37d2f0
fjuniorr <fjunior.alves.oliveira@gmail.com>
2018-09-10 14:28:03 -0300

    Adiciona licensa MIT e documentacao

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,8 @@
 #' Hello Function
 #' This functions is a hello word function which aims
 #' at giving some instructions
+#'
+#' @param x A string
 #' @export
 hello <- function(x) {
   glue::glue("Hello {x}!")
