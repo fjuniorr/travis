6de18b2ca63535fd53fe7ac7d7a1e87b0c3e71ee
fjuniorr <fjunior.alves.oliveira@gmail.com>
2019-01-24 15:41:21 -0200

    Misc commit

--- tests/testthat/test_hello.R
+++ tests/testthat/test_hello.R
@@ -3,3 +3,16 @@ context("Automated Testing")
 test_that("hello functions works properly", {
   expect_match(hello("Francisco"), "Hello Francisco!", fixed = TRUE)
 })
+
+test_that("fail test", {
+  expect_true(FALSE)
+})
+
+test_that("pass test", {
+  expect_true(TRUE)
+})
+
+
+test_that("another fail", {
+  expect_false(TRUE)
+})
